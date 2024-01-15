05445fc3ceb225e5d3b9792f9225989da16a485a
jinwoo0207 <a91287840@gmail.com>
2024-01-15 11:26:06 +0900

    돔을 이용하여 html 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,21 @@
 "use strict"
+//dom --> 문서 객체 모델
+// 4줄 : 질의 선택자 html에서 선택자를 지정하면 값을 가져올 수 있다.
+const id = document.querySelector("#id"), //#은 input id = "id" <- 이 값을 가져온다는 뜻  
+    psword = document.querySelector("#psword"),
+    loginBut = document.querySelector("button");
 
-console.log("하이");
-console.log("바이");
+loginBut.addEventListener("click", login);
+
+function login(){
+    const req = {
+        id : id.value,
+        psword : psword.value,
+    };
+    console.log(req);
+}
+
+
+
+
+
