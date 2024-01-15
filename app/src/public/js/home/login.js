7d15cd8519020945eb7b58e88248e3f0cb25f832
jinwoo0207 <a91287840@gmail.com>
2024-01-15 19:34:58 +0900

    백엔드에서 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,31 +1,24 @@
-"use strict"
+"use strict";
 
-// const { json } = require("body-parser");
+const id = document.querySelector("#id"),
+   psword = document.querySelector("#psword"),
+   loginBtn = document.querySelector("button");
 
-//dom --> 문서 객체 모델
-// 4줄 : 질의 선택자 html에서 선택자를 지정하면 값을 가져올 수 있다.
-const id = document.querySelector("#id"), //#은 input id = "id" <- 이 값을 가져온다는 뜻  
-    psword = document.querySelector("#psword"),
-    loginBut = document.querySelector("button");
+loginBtn.addEventListener("click", login);
 
-loginBut.addEventListener("click", login);
+function login() {
+   const req = {
+      id: id.value,
+      psword: psword.value,
+   };
 
-function login(){
-    const req = {
-        id : id.value,
-        psword : psword.value,
-    };
-    fetch("/login", {
-        method : "POST",
-        headers : {
-            "Content-Type" : "application/json",
-        },
-        body : JSON.stringify(req)
-    }).then((res) => res.json())
-    .then(console.log);
+   fetch("/login", {
+      method: "POST",
+      headers: {
+         "Content-Type": "application/json",
+      },
+      body: JSON.stringify(req),
+   })
+   .then((res) => (res.json()))
+   .then(console.log);
 }
-
-
-
-
-
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -4,38 +4,39 @@ const output = {
     hello: (req, res) => {
         res.render("home/index");
     },
-    
+
     login: (req, res) => {
         res.render("home/login");
     },
 };
 
 const users = {
-    id: ["jinwoo", "진우", "ajw"],
-    psword: ["0724", "1234", "1234"],
+    id: ["jinwoo", "ajw", "진우"],
+    psword: ["0724", "1234", "12345"],
 };
 
 const process = {
     login: (req, res) => {
-        const id = req.body.id;
-        const psword = req.body.psword;
+        const id = req.body.id,
+        psword = req.body.psword;
 
-        if(users.id.includes(id)) {
+        if (users.id.includes(id)) {
             const idx = users.id.indexOf(id);
-            if(users.psword[idx] === psword) {
+            if (users.psword[idx] === psword) {
                 return res.json({
                     success: true,
                 });
             }
         }
-        return res.json({
+
+        return res.kson({
             success: false,
-            msg: "로그인 실패!",
-        });
+            msg: "로그인에 실패하셨습니다.",
+        })
     },
 };
 
 module.exports = {
     output,
-    process,    
+    process,
 };
