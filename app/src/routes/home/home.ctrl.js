572f562ffb9c13570006bc83a72b3d047b9f2e82
jinwoo0207 <a91287840@gmail.com>
2024-01-17 13:21:30 +0900

    회원가입 페이지 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,5 @@
 "use strict";
 
-
 const User = require("../../models/User"); 
 const output = {
     hello: (req, res) => {
@@ -10,8 +9,10 @@ const output = {
     login: (req, res) => {
         res.render("home/login");
     },
+    register : (req, res) => {
+        res.render("home/register")
+    }
 };
-
 
 const process = {
     login: (req, res) => {
